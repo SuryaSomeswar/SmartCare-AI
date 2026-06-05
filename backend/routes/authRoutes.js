3f11488afdd2dcdfb29665f9c6ce23572d6db0b2
SHANMUKHA VENKATA SURYA SOMESWAR GUDLA <gudlasuryasomeswar02@gmail.com>
2026-06-05 16:16:56 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,6 +9,41 @@ router.post("/register", async (req, res) => {
   try {
     const { name, email, password } =
       req.body;
+    if (
+  password.length < 8
+) {
+  return res.status(400).json({
+    message:
+      "Password must be at least 8 characters long",
+  });
+}
+
+if (
+  !/[A-Z]/.test(password)
+) {
+  return res.status(400).json({
+    message:
+      "Password must contain at least one uppercase letter",
+  });
+}
+
+if (
+  !/[a-z]/.test(password)
+) {
+  return res.status(400).json({
+    message:
+      "Password must contain at least one lowercase letter",
+  });
+}
+
+if (
+  !/[0-9]/.test(password)
+) {
+  return res.status(400).json({
+    message:
+      "Password must contain at least one number",
+  });
+}
     const existingUser =
   await User.findOne({ email });
 
